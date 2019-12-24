fbc6c55f05fac5bf315197c6fab81340dbc85664
ma-558 <58020381+ma-558@users.noreply.github.com>
2019-12-24 11:49:52 +0900

    デプロイ　テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
   def index
     render html: "hello world"
+  end
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["hello world"] && password == ENV["6565"]
+    end
   end
 end
