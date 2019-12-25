f485acb0ceed5303afd51e0c4a2325bd70d9d0dc
ChingHuaHsieh <link0319@yahoo.com.tw>
2019-12-25 12:51:13 +0900

    add_top_page

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+
+  def index
+    @parents = Category.all.order("id ASC").limit(13)
+
+  end
+
+  def show
+    @parents = Category.all.order("id ASC").limit(13)
+  end
+end
