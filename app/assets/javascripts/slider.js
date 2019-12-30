28747e3204c1140b0c5b8ab1676d545af3c1b59f
ChingHuaHsieh <link0319@yahoo.com.tw>
2019-12-30 15:28:22 +0900

    add_when_login_and_soldout_view

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,3 +1,5 @@
+  //  top_page  //
+
 $(function() {
   $('.slider').slick({
       prevArrow:'.slick-prev',
@@ -13,10 +15,13 @@ $(function() {
   });
 });
 
+ //  product_show  //
+
 $(function() {
   $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplaySpeed: 3500,
     arrows: false,
     fade: true,
     asNavFor: '.slider-nav'
@@ -25,6 +30,6 @@ $(function() {
     slidesToShow: 5,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
-    focusOnSelect: true
+    focusOnSelect: true,
   });
 });
