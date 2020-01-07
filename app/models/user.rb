ed060eda0458f026bffa73a7edb3daf62bb3288c
kin0ako <email@example.com>
2020-01-07 20:05:52 +0900

    identification

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
   has_many :products
   has_many :credit_cards
   has_one :delivery_info
+
+  def full_name
+    "#{self.l_name} #{self.f_name}"
+  end
+
+  def full_name_kana
+    "#{self.l_name_kana} #{self.f_name_kana}"
+  end
+
+  def birthday
+    "#{BirthYyyy.find(self.birth_yyyy_id).year}/#{BirthMm.find(self.birth_mm_id).month}/#{BirthDd.find(self.birth_dd_id).day}"
+  end
 end
