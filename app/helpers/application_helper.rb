92e600d777f8da89b431afbbc98292042e11e7b5
kin0ako <email@example.com>
2020-01-07 20:37:25 +0900

    identification3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def full_name
+    "#{self.l_name} #{self.f_name}"
+  end
+
+  def full_name_kana
+    "#{self.l_name_kana} #{self.f_name_kana}"
+  end
+
+  def birthday
+    "#{BirthYyyy.find(self.birth_yyyy_id).year}/#{BirthMm.find(self.birth_mm_id).month}/#{BirthDd.find(self.birth_dd_id).day}"
+  end
 end
